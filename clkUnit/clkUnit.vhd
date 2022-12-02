1db0f75c1b6340606892a125f2f313d907364b4a
remenby <baptiste.cruvellier31@gmail.com>
2022-12-02 14:42:17 +0100

    clkunit

--- clkUnit/clkUnit.vhd
+++ clkUnit/clkUnit.vhd
@@ -12,6 +12,30 @@ end clkUnit;
 
 architecture behavorial of clkUnit is
 
+  constant facteur : natural := 16;
+
 begin
+
+  enableRX <= clk;
+
+  div : process (clk, reset)
+    variable cpt : integer range 0 to facteur-1 := 0;
+  begin 
+    if reset = '0' then
+      enableTX <= '0';
+      cpt := 0;
+    elsif rising_edge(clk) then
+      if(cpt = facteur - 1) then
+        cpt := 0;
+      else
+        cpt := cpt + 1;
+      end if;
+      if cpt = 0 then
+        enableTX <= '1';
+      else
+        enableTX <= '0';
+      end if;
+    end if;
+  end process;
 
 end behavorial;
